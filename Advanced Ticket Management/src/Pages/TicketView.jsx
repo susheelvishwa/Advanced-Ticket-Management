c0a09b9df22de69d2a1e61f619928a09068f0708
susheelvishwakarma <susheelvishwakarma.india@gmail.com>
2024-08-01 00:04:11 +0530

    delete functionality

--- Advanced Ticket Management/src/Pages/TicketView.jsx
+++ Advanced Ticket Management/src/Pages/TicketView.jsx
@@ -1,4 +1,4 @@
-import { Button, Box, Text, Flex, Heading} from "@chakra-ui/react";
+import { Button, Box, Text, Flex, Heading } from "@chakra-ui/react";
 import { useParams, useNavigate } from "react-router-dom";
 import { useState, useEffect } from "react";
 import axios from "axios";
@@ -35,10 +35,10 @@ const ViewTicket = () => {
       await axios.delete(
         `https://react-final-project-rouge.vercel.app/tickets/${id}`
       );
-      navigate("/tickets");
     } catch (error) {
       console.error("Error deleting the ticket:", error);
     }
+    navigate("/tickets");
   };
 
   if (loading) return <LoadingIndicator />;
